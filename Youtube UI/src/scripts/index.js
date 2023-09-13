add987bb79d18d008e871b52bc0bb2390cbb4d22
arjunpras <arjun.prasad@team.telstra.com>
2023-09-14 01:24:10 +0530

    side bar fix

--- Youtube UI/src/scripts/index.js
+++ Youtube UI/src/scripts/index.js
@@ -1,28 +1,28 @@
 const menuButton = document.getElementById('menu');
 const sideBar = document.querySelector('.side-bar');
+const sideBarNav = document.querySelector('.nav');
 const sideBarATag = document.querySelectorAll(".nav-link");
+const sideBarSpanTag = document.querySelectorAll(".nav-text");
 let toggleMenu = 1;
 
 const handleOnClick = () => {
     if (toggleMenu) {
         sideBar.classList = "side-bar-collapsed";
         sideBarATag.forEach(tag => {
-            tag.style = `
-                flex-direction: column;
-                justify-content: center;
-                align-item: center;
-            `;
+            tag.classList = "nav-link-collapsed";
+        });
+        sideBarSpanTag.forEach(tag => {
+            tag.classList = "nav-text-collapsed";
         });
         toggleMenu = 0;
     }
     else {
         sideBar.classList = "side-bar";
         sideBarATag.forEach(tag => {
-            tag.style = `
-                flex-direction: row;
-                justify-content: flex-start;
-                align-item: center
-            `;
+            tag.classList = "nav-link"
+        });
+        sideBarSpanTag.forEach(tag => {
+            tag.classList = "nav-text"
         });
         toggleMenu = 1;
     }
